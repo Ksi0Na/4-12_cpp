bbcd3f40ef97db86a8731b260a19268d4e293488
Ksi0Na <OlgaMolchun20010615@yandex.ru>
2021-07-09 09:09:55 +0300

    0

---
+++ lab-12-refactoring-master/sources/log.cpp
@@ -0,0 +1,15 @@
+// Copyright 2020 Ksi0Na
+
+#include <log.hpp>
+
+void log_t::write(std::string_view message) const {
+  *_output << message << '\n';
+}
+
+void log_t::write_debug(std::string_view message) const {
+  if (!_level) *_output << message << '\n';
+}
+log_t* log_t::get_instance() {
+  static log_t instance{};
+  return &instance;
+}
---
+++ lab-12-refactoring-master/tests/stat_sender_test.hpp
@@ -0,0 +1,12 @@
+
+#pragma once
+
+#include <gmock/gmock.h>
+#include <stat_sender.hpp>
+
+class mock_stat_sender : public stat_sender {
+  //  virtual void async_send(const std::vector<item>&,
+  //                          std::string_view);
+ public:
+  MOCK_METHOD2(async_send, void(const std::vector<item>&, std::string_view));
+};
